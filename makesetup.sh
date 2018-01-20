fe84472015b4a72d5a1ef6de9a383c04dcbde13b
yabra <bagyar@taximaxim.ru>
2018-01-20 23:31:50 +0500

    have sawn off the excess

--- makesetup.sh
+++
@@ -1,4 +0,0 @@
-version=`sed -n 's/^version *= *//p' build.gradle`
-echo Building setup for $version
-sed -i "s/#define MyAppVersion .*/#define MyAppVersion $version/" RaspberryJamMod.iss
-"C:/Program Files (x86)/Inno Setup 5/compil32" /cc RaspberryJamMod.iss
--- mccolors/go.sh
+++
@@ -1,2 +0,0 @@
-unzip $APPDATA/.minecraft/versions/1.10.2/1.10.2.jar 'assets/minecraft/textures/blocks/*.*'
-python getcolors.py > colordictionary.py
